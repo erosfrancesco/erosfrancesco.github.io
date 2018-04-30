e99f7dd1ca7eb145ac2ec4332c2784442fa8a49c
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-04-30 20:00:37 +0200

    Delete index.js

--- layer2/index.js
+++
@@ -1,6 +0,0 @@
-var _Array_Index_Load_ = [ 	'layer2/battle_menu',
-							'layer2/stats', 
-							'layer2/player_manager',
-	                        'layer2/battle_manager',
-							'layer2/target'
-];
