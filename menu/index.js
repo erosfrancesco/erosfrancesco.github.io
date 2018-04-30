cdcc7064ccc99465274f9bf3d129f82340e5ee8b
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-04-30 22:33:06 +0200

    Delete index.js

--- menu/index.js
+++
@@ -1,9 +0,0 @@
-var _Array_Index_Load_ = [ 	'menu/setting',
-							'menu/cursor',
-							'menu/scroller',
-                            'menu/menu_input',
-                            'menu/menu_battle_objects', 
-                            'menu/menu_headers', 
-                            'menu/menu_DOM',
-                            //'menu/banner'
-];
