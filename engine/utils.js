1c94ca010843eb7c4f3c075f15128e2ec375d155
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-08-11 20:13:31 +0200

    Add files via upload

---
+++ engine/utils.js
@@ -0,0 +1,36 @@
+const __PAUSEEVENTKEYBOARDKEY = 'keydown_P';
+
+
+/**/
+function deepClone(o){
+	
+	let _out = Array.isArray(o) ? [] : {};
+	
+	Object.keys(o).forEach(_key=> {
+		let v = o[_key];
+		_out[_key] = (typeof v === "object") ? deepClone(v) : v;
+	});
+	
+	return _out;
+}
+/**/
+
+
+function PauseScene(scene) {
+            scene.pause();
+            scene.scene.sound.sounds.forEach(sound => sound.pause());
+        }
+        function ResumeScene(scene) {
+            scene.resume();
+            scene.scene.sound.sounds.forEach(sound => sound.resume());
+        }
+
+        function TogglePause(scene) {
+            if (scene.Paused) {
+                scene.Paused = false;
+                ResumeScene(scene);
+            }else{
+                scene.Paused = true;
+                PauseScene(scene);
+            }
+        }
