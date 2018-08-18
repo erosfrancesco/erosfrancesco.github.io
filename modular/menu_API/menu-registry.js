c3755bc253986f942cea0e2daeec2d07d6b2e5ef
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-08-18 21:24:46 +0200

    Add files via upload

---
+++ modular/menu_API/menu-registry.js
@@ -0,0 +1,50 @@
+export default class MenuRegistry {
+    constructor(options) {
+        options = options || {};
+        let {menus, keep, onDestroy, onAdd} = options;
+
+        this.onDestroy = onDestroy || function (menu, callback) { callback(); }
+        this.onAdd = onAdd || function (menu, callback) { callback(); }
+
+        this._keep = keep || 0;
+        this._menuList = menus || [];
+    }
+
+    add(menu)  {
+        this.onAdd(menu, () => this._menuList.push(menu) );
+    }
+
+    remove() {
+        if ( this._menuList[this._keep] ) {
+            this.onDestroy(this.current, () => this._menuList.pop() );
+        } 
+    }
+
+    reset() {
+        while( this._menuList[this._keep] ) { this.remove(); }
+    }
+
+    get length() {
+        return this._menuList.length;
+    }
+
+    get current() {
+        return this._menuList[this._menuList.length - 1];
+    } 
+
+    get onAdd() {
+        return this._onAdd;
+    }
+
+    set onAdd(v) {
+        this._onAdd = v;
+    }
+
+    get onDestroy() {
+        return this._onRemove;
+    }
+
+    set onDestroy(v) {
+        this._onRemove = v;
+    }
+}
