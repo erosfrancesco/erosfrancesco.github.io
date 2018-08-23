b18452018cff60b5e6eae5c6c7753214d366a9c7
eros.francesco <eros.francesco.francesco@gmail.com>
2018-08-23 01:22:13 +0100

    'changing'
    hello changing

--- threejs/index.js
+++
@@ -1,8 +0,0 @@
-var _Array_Index_Load_ = [  'threejs/minecraftThree',
-                            'threejs/Detector', 
-                            'threejs/THREEx.KeyboardState', 
-                            'threejs/Stats',
-                            'threejs/touch',
-                            'threejs/ImprovedNoise',
-                            'threejs/FirstPersonControls'
-];
