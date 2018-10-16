47edeaa7d739682a5c9a501201a88ca764909339
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-10-16 16:10:01 +0200

    Update readme.js

--- modular/battle_utils/readme.js
+++ modular/battle_utils/readme.js
@@ -1,3 +1,6 @@
-Add BattleAnimator to atbBattle
-Fu**ing BattleAnimator had a bug with removeFirstAction method. Took time to figure that out!
+#Work in progress
+
+##Bugs
+F*** BattleAnimator. Took time to figure that out!
+  removeFirstAction
 
