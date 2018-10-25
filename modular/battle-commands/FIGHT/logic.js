cee4c149116b469e086bc377ed5b93c49d93f61d
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-10-25 14:28:34 +0200

    Update logic.js

--- modular/battle-commands/FIGHT/logic.js
+++ modular/battle-commands/FIGHT/logic.js
@@ -1,26 +1,47 @@
-import FightAction from './animation-fight.js';
+import BattleDamage from '../battle_utils/battle-damage.js';
 
+export default class ComputeDamage extends BattleDamage {
+    constructor(options, exec, target) {
 
+        // intial settings
+        options.value = 30;
+        options.blunt = true;
+        
+        // compute exec and target
+        const atkRoll = Phaser.Math.Between(1, exec.Stats.get('strength') ) * 2;
+        const defRoll = Phaser.Math.Between(1, target.Stats.get('defense') ) * 2;
+        options.value -= defRoll;
+        options.value += atkRoll;
+        options.value = (options.value < 0) ? 0 : options.value;
+        
+        super(options);
+        
+        Object.keys(this.types).forEach(type => {
+            if (target.events.onDamageType[type])
+            target.events.onDamageType[type](this);
+        });
+    }
+}
+
+/*
 export function ComputeDamage(exec, target) {
 
     let value = 30;
-    let atkRoll = Phaser.Math.Between(1, exec.Stats.get('strength') ) * 2;
-    let defRoll = Phaser.Math.Between(1, target.Stats.get('defense') ) * 2;
+    const atkRoll = Phaser.Math.Between(1, exec.Stats.get('strength') ) * 2;
+    const defRoll = Phaser.Math.Between(1, target.Stats.get('defense') ) * 2;
     
     value -= defRoll;
     value += atkRoll;
     value = (value < 0) ? 0 : value;
     
 
-    let damage = new BattleDamage({value, blunt: true});
+    const damage = new BattleDamage({value, blunt: true});
 
     Object.keys(damage.types).forEach(type => {
         if (target.events.onDamageType[type])
         target.events.onDamageType[type](damage);
     });
-
-    console.log(damage);
-
+    
     return damage;
 }
-
+/**/
