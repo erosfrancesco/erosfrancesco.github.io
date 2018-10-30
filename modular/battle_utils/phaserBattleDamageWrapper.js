a3d803b6232f7f9c3a4c68b29e29588052479172
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-10-30 16:00:30 +0100

    Update phaserBattleDamageWrapper.js

--- modular/battle_utils/phaserBattleDamageWrapper.js
+++ modular/battle_utils/phaserBattleDamageWrapper.js
@@ -1,1 +1,16 @@
+import BattleDamage  from './battle-damage.js';
 
+export default PhaserDamage() {
+    constructor(options) {
+        const {value, types, Sprite} = options;
+        types.value = value;
+        super(types);
+        this.Sprite = Sprite;
+    }
+    
+    display() {
+        // new Phaser text
+        // compute x, y
+        // display
+    }
+}
