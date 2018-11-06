5044836d928b909d34228d92171f84f3a086064a
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-11-06 12:21:21 +0100

    Update index.js

--- phaser_scenes/index.js
+++ phaser_scenes/index.js
@@ -1,1 +1,28 @@
+class PauseScene extends Phaser.Scene {
+	constructor() { 
+    const config = { key: 'PauseScene' }; 
+    super(config); 
+  }
+    create() { 
+      // on input
+    }
+}
 
+const config = {
+	type: Phaser.AUTO,
+	width: 900,
+	height: 500,
+	backgroundColor: '#444',
+  // should create game-container
+	parent: "game-container",
+	scene: [PauseScene],
+  
+	physics: {
+		default: "arcade",
+		arcade: {
+			gravity: { y: 0 }
+		}
+	}
+};
+
+const game = new Phaser.Game(config);
