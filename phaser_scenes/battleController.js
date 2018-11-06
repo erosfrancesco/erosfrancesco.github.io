1444f19967c4136b2b76f44600a939fbbdd88925
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-11-06 12:43:27 +0100

    Update battleController.js

--- phaser_scenes/battleController.js
+++ phaser_scenes/battleController.js
@@ -6,34 +6,34 @@ class BattleControllerScene extends Phaser.Scene {
 	}
 	
 	create() {
-// INPUT
-this.input.keyboard.on('keyup_UP', event => {
-	this.events.emit('__global_pressed_up');
-}, this);
+		// INPUT
+		this.input.keyboard.on('keyup_UP', event => {
+			this.events.emit('__global_pressed_up');
+		}, this);
 
-this.input.keyboard.on('keyup_DOWN', event => {
-	this.events.emit('__global_pressed_down');
-}, this);
+		this.input.keyboard.on('keyup_DOWN', event => {
+			this.events.emit('__global_pressed_down');
+		}, this);
 
-this.input.keyboard.on('keyup_LEFT', event => {
-	this.events.emit('__global_pressed_left');
-}, this);
+		this.input.keyboard.on('keyup_LEFT', event => {
+			this.events.emit('__global_pressed_left');
+		}, this);
 
-this.input.keyboard.on('keyup_RIGHT', event => {
-	this.events.emit('__global_pressed_right');
-}, this);
+		this.input.keyboard.on('keyup_RIGHT', event => {
+			this.events.emit('__global_pressed_right');
+		}, this);
 
-this.input.keyboard.on('keyup_Z', event => {
-	this.events.emit('__global_pressed_z');
-}, this);
+		this.input.keyboard.on('keyup_Z', event => {
+			this.events.emit('__global_pressed_z');
+		}, this);
 
-this.input.keyboard.on('keyup_X', event => {
-	this.events.emit('__global_pressed_x');
-}, this);
+		this.input.keyboard.on('keyup_X', event => {
+			this.events.emit('__global_pressed_x');
+		}, this);
 
-// MENU REGISTRY
-this.MenuRegistry = new MenuRegistry({});
-this.MenuPool = {};
+		// MENU REGISTRY
+		this.MenuRegistry = new MenuRegistry({});
+		this.MenuPool = {};
 	}
 	
 	buildMenu(id, config) {
@@ -43,5 +43,15 @@ this.MenuPool = {};
 	
 	loadMenu(id, Menu) {
 		this.MenuPool[id] = Menu;
+	}
+	
+	removeMenu() {
+		this.MenuRegistry.remove();
+	}
+	
+	removeAllMenu() {
+		while(this.MenuRegistry.size) {
+			this.MenuRegistry.remove();
+		}
 	}
 }
