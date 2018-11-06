cefce0857e230f7943105532fcb5e8f69715569a
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-11-06 12:32:05 +0100

    Update battleController.js

--- phaser_scenes/battleController.js
+++ phaser_scenes/battleController.js
@@ -1,1 +1,32 @@
-
+class BattleControllerScene extends Phaser.Scene {
+	constructor() { 
+		const config = { key: 'BattleControllerScene' }; 
+		super(config); 
+	}
+	
+	create() { 
+		this.input.keyboard.on('keyup_UP', event => {
+       		this.events.emit('__global_pressed_up');
+		}, this);
+    
+    this.input.keyboard.on('keyup_DOWN', event => {
+       		this.events.emit('__global_pressed_down');
+		}, this);
+    
+    this.input.keyboard.on('keyup_LEFT', event => {
+       		this.events.emit('__global_pressed_left');
+		}, this);
+    
+    this.input.keyboard.on('keyup_RIGHT', event => {
+       		this.events.emit('__global_pressed_right');
+		}, this);
+    
+    this.input.keyboard.on('keyup_Z', event => {
+       		this.events.emit('__global_pressed_z');
+		}, this);
+    
+    this.input.keyboard.on('keyup_X', event => {
+       		this.events.emit('__global_pressed_x');
+		}, this);
+	}
+}
