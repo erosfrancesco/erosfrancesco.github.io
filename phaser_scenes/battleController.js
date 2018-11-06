97b4b571fbe57ca564ecf55d5f6f643b060f91f2
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-11-06 12:41:13 +0100

    Update battleController.js

--- phaser_scenes/battleController.js
+++ phaser_scenes/battleController.js
@@ -5,29 +5,43 @@ class BattleControllerScene extends Phaser.Scene {
 		super(config); 
 	}
 	
-	create() { 
-		this.input.keyboard.on('keyup_UP', event => {
-			this.events.emit('__global_pressed_up');
-		}, this);
-    
-	    this.input.keyboard.on('keyup_DOWN', event => {
-			this.events.emit('__global_pressed_down');
-		}, this);
+	create() {
+// INPUT
+this.input.keyboard.on('keyup_UP', event => {
+	this.events.emit('__global_pressed_up');
+}, this);
 
-	    this.input.keyboard.on('keyup_LEFT', event => {
-			this.events.emit('__global_pressed_left');
-		}, this);
+this.input.keyboard.on('keyup_DOWN', event => {
+	this.events.emit('__global_pressed_down');
+}, this);
 
-	    this.input.keyboard.on('keyup_RIGHT', event => {
-			this.events.emit('__global_pressed_right');
-		}, this);
+this.input.keyboard.on('keyup_LEFT', event => {
+	this.events.emit('__global_pressed_left');
+}, this);
 
-	    this.input.keyboard.on('keyup_Z', event => {
-			this.events.emit('__global_pressed_z');
-		}, this);
+this.input.keyboard.on('keyup_RIGHT', event => {
+	this.events.emit('__global_pressed_right');
+}, this);
 
-	    this.input.keyboard.on('keyup_X', event => {
-       		this.events.emit('__global_pressed_x');
-		}, this);
+this.input.keyboard.on('keyup_Z', event => {
+	this.events.emit('__global_pressed_z');
+}, this);
+
+this.input.keyboard.on('keyup_X', event => {
+	this.events.emit('__global_pressed_x');
+}, this);
+
+// MENU REGISTRY
+this.MenuRegistry = new MenuRegistry({});
+this.MenuPool = {};
+	}
+	
+	buildMenu(id, config) {
+		const Menu = this.MenuPool[id];
+		this.MenuRegistry.add( Menu.build(config) );
+	}
+	
+	loadMenu(id, Menu) {
+		this.MenuPool[id] = Menu;
 	}
 }
