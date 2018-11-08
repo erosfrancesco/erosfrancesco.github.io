77f50e0fec77d9703a075ca4968abe9b77eb1f65
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-11-08 10:00:38 +0100

    Update BattleScene.js

--- phaser_scenes/BattleScene.js
+++ phaser_scenes/BattleScene.js
@@ -1,1 +1,45 @@
+export default class BattleScene extends Phaser.Scene {
 
+    constructor({inputSceneKey}) {
+        
+        super("Battle");
+      
+        this.Animator = new ActionRegistry();
+
+        this.Enemies = new CharacterRegistry();
+        this.Players = new CharacterRegistry();
+        this.inputScene = inputSceneKey;
+    }
+
+    // forEach
+    forAllCharacters(f) {
+        this.Enemies.forEach((character, index) => f(character, index) );
+        this.Players.forEach((character, index) => f(character, index) );
+    }
+    
+    // input from
+    set inputScene(key) {
+        this._inputSceneKey = key;
+    }
+    
+    get inputScene() {
+        return this.scene.getScene(this._inputSceneKey);
+    }
+
+    // set current player and enemy
+    update(callback) {
+        
+        // resolve actions
+        if (this.Animator.busy) { return; }
+        if (this.Animator.hasAction) { this.Animator.resolve(); return; }
+
+        // update character atb
+        this.forAllCharacters(c => c.TurnSystem.update(() => this.onCharacterUpdate(c) ) );
+        
+        // manage turns
+        ManageRegistryTurn(this, this.Players);
+        ManageRegistryTurn(this, this.Enemies);
+
+        callback();
+    }
+}
