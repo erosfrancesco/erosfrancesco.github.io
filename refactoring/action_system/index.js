f863f3aa028bca660dc842c261d66721b0367985
eros francesco reale <eros.francesco.francesco@gmail.com>
2018-11-14 12:02:27 +0100

    Update index.js

--- refactoring/action_system/index.js
+++ refactoring/action_system/index.js
@@ -1,4 +1,4 @@
 import ActionRegistry from './actionRegistry.js';
 import TurnActionRegistry from './animatorBattleBridge.js';
-
+// PlayerTurnActionRegistry
 export default {ActionRegistry, TurnActionRegistry};
