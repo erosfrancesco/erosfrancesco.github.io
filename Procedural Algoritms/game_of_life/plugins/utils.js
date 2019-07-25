1a42499aa4676b6bb175df22a16eff50403e6235
eros francesco reale <eros.francesco.francesco@gmail.com>
2019-07-25 17:53:53 +0200

    Add files via upload

--- Procedural Algoritms/game_of_life/plugins/utils.js
+++ Procedural Algoritms/game_of_life/plugins/utils.js
@@ -1,9 +1,8 @@
-// import VectorCell from './vectorCell.js';
+import VectorCellsTrail from './vectorCell.js';
 import GoL from '../gol.js';
 const {Layer} = GoL
 
-//function GoLLayer (width = 100, height = 100, cellPropertiesDistribution = function(x, y) { return []; })
-
+// Conversion
 function convertArrayToLayer(w, h, data, conversion = function(value) { return [{name:"valueConverted", value}]; }) {
 	if (data.length !== w * h) {
 		console.log("nope!", data.length + " is not multiple of " + w + " and " + h)
@@ -22,10 +21,58 @@ function convertLayerToTilemap(layer, conversion = function(cell) { return 0 }) 
 	});
 
 	return data;
-};
+}
+
+class FilterBucket {
+	constructor(layer, filter) {
+		this.layer = layer
+		this.filter = filter
+		
+		this.usedFilters = []
+		this.saved = []
+		
+	}
+
+	getStaticClusterOf(x, y) {
+
+		const clusterIndex = this.saved.findIndex(cluster => {
+			let ret = false
+			cluster.cells.forEach(cell => {
+				if (cell.x === x && cell.y === y) {
+					ret = true
+					return ret
+				}
+			})
+			return ret
+		});
+		
+		if (clusterIndex > -1) {
+			return this.saved[clusterIndex]
+		}
+		
+		const cluster = this.getClusterOf(x, y, this.filter)
+		const cells = []
+		cluster.trail.forEach(vectorCell => cells.push(vectorCell.currentCell) )
+		this.saved.push({cluster, cells})
+
+		return this.saved[this.saved.length - 1]
+	}
+
+	getClusterOf(x, y) {
+
+		const trail = new VectorCellsTrail(this.layer, this.layer.getCell(x, y), this.filter);
+
+		while (trail.updateTrail()) {
+			// console.log("updating")			
+		}
+
+		return trail
+	}
+}
 
 
 export default {
 	convertArrayToLayer,
-	convertLayerToTilemap
+	convertLayerToTilemap,
+	FilterBucket
 }
